3fd0df575b359a49559727bd98f783500b97f4fc
Ishank <kashu.12569@gmail.com>
2018-03-23 15:56:05 +0530

    change commit

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Hello world!</title>
+	<title>Hello world! This is Build 2</title>
 	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
 	body {
